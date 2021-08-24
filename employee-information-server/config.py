cd5908b8ff13d200a6168097323f63634a92833a
Dominik <admin@dominik48n.de>
2021-08-24 16:10:48 +0200

    Create config

---
+++ employee-information-server/config.py
@@ -0,0 +1,7 @@
+# MySQL Database connection data
+mysql = {
+    "host": "mysql-host",
+    "user": "employee-information",
+    "password": "mysql-password",
+    "database": "employee-information",
+}
